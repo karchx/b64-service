39144567cfc7b96bba39ab6429d3de35e1ae658f
Стив <kenethra.74@gmail.com>
2023-02-14 14:34:49 -0600

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/karchx/b64-service
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"encoding/base64"
+	"encoding/json"
+	"os"
+	"log"
+)
+
+type ResponseData struct {
+	Data string `bson:"data" json:"data"`
+}
+
+func main() {
+	http.HandleFunc("/", serveFile)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+
+func serveFile(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open("../facturas/FACT_002D81A0_2635155854.pdf")
+
+	if err != nil {
+		log.Printf("%s: ", err)
+		http.Error(w, "Can't open file", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Printf("%s: ", err)
+		http.Error(w, "Can't open file", http.StatusInternalServerError)
+		return
+	}
+
+	fileResponse := ResponseData{
+		Data:  base64.StdEncoding.EncodeToString(data),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(fileResponse)
+}
+
+func filterFile() {}
